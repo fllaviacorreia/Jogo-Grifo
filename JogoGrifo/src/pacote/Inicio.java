7b1f319ac1d78ebd5af7cfe8e69316065b035031
EdeerCarloos <edeer.santoos11@gmail.com>
2019-06-12 13:12:02 -0300

    Eder alterou os dados
    Jogo Java

--- JogoGrifo/src/pacote/Inicio.java
+++ JogoGrifo/src/pacote/Inicio.java
@@ -15,6 +15,7 @@ public class Inicio extends JFrame {
 	private JLabel fundo;
 	private JButton inicio;
 	//mudei aqui 
+	//Eu Eder mudei aqui também 
 	public Inicio() {		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1024, 800);
